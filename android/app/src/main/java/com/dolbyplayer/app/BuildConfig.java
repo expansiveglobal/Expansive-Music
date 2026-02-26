f1b470c2947dec4c4938a3f6dbfe5727ae896bc0
greatestofall <greatestofall@noreply.codeberg.org>
2026-02-26 17:36:30 +0100

    android/app/src/main/java/com/dolbyplayer/app/BuildConfig.java toevoegen
    package com.dolbyplayer.app;
    
    public final class BuildConfig {
        public static final boolean DEBUG = Boolean.parseBoolean("false");
        public static final String APPLICATION_ID = "com.dolbyplayer.app";
        public static final String BUILD_TYPE = "release";
        public static final int VERSION_CODE = 1;
        public static final String VERSION_NAME = "1.0";
    }

---
+++ android/app/src/main/java/com/dolbyplayer/app/BuildConfig.java
@@ -0,0 +1,9 @@
+package com.dolbyplayer.app;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("false");
+    public static final String APPLICATION_ID = "com.dolbyplayer.app";
+    public static final String BUILD_TYPE = "release";
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "1.0";
+}
